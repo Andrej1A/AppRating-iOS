0748d8f3078365be3a812237c3516206b0754602
Andrej Albrecht <andrejalbrecht@gmx.de>
2016-09-04 06:42:34 -0500

    Initial Commit

---
+++ AppRatingiOS/AppRatingiOS.h
@@ -0,0 +1,19 @@
+//
+//  AppRatingiOS.h
+//  AppRatingiOS
+//
+//  Created by Andrej Albrecht on 04.09.16.
+//  Copyright © 2016 Andrej Albrecht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppRatingiOS.
+FOUNDATION_EXPORT double AppRatingiOSVersionNumber;
+
+//! Project version string for AppRatingiOS.
+FOUNDATION_EXPORT const unsigned char AppRatingiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppRatingiOS/PublicHeader.h>
+
+
